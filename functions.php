8578c5cf6f33b43dc8bf157a43c9ebaf89cbaab0
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-09-02 16:27:34 +0200

    Adds load_javascript function into functions.php - creates a custom function to load javascript in the custom theme

--- functions.php
+++ functions.php
@@ -7,3 +7,10 @@ function load_stylesheets()
 } 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
+function load_javascript()
+{
+    wp_register_script('custom', get_template_directory_uri() . '/app.js', '', 1, true);
+    wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
+
