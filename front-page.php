3bd5836c34dd4458a9b46d83f5578942e2a34422
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-09-07 16:38:28 +0200

    Add function to insert content from WP editor to the template - add into front-page-php functions that behave as a loop to insert content from the WP editor to the page template

--- front-page.php
+++ front-page.php
@@ -5,7 +5,10 @@
         <h1 class="hero__title">Tejidos Europa</h1>
     </section>
     <section class="content">
-        
+        <?php if(have_post()) : while(have_post()) : the_post();?>
+        <?php the_content();?>
+        <?php endwhile; else; endif;?>
+
     </section>
 </main>
 
