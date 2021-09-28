b4a5ca62b918d9106c4f746dbf2676673e295a92
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-09-28 17:05:58 +0200

    feat footer links: add mailto and tel links to footer email and phone number, respectively. Also add the payment methods picture

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
           <div class="footer__cols__col">
             <p class="footer__cols__title">Ayuda y Contacto</p>
             <a href="http://tejidoseuropa.test/contacto/">Solicitar información</a>
-            <p>+34 932 13 19 04</p>
-            <p>tejidoseuropa@tejidoseuropa.com</p>
+            <a href="tel:+34 932 13 19 04">+34 932 13 19 04</a>
+            <a href="mailto:tejidoseuropa@tejidoseuropa.com">tejidoseuropa@tejidoseuropa.com</a>
           </div>
           <div class="footer__cols__col"> 
             <p class="footer__cols__title">Sobre Tejidos Europa</p>
@@ -22,13 +22,7 @@
           </div>
           <div class="footer__cols__col">
             <p class="footer__cols__title">Nuestros modos de pago</p>
-            <?php
-              wp_nav_menu(
-                array(
-                  'theme_location' => 'footer-center',
-                  'menu_class' => 'split-y'
-                )
-              ); ?>
+            <img src="<?php bloginfo('template_directory');?>/images/metodos-pago.png" class="footer__logo">
           </div>
         </div>
         <div class="footer__legal">
