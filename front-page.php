de11977ed5ccbc4030f2296f5bfbbf4fcc90fe79
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-09-29 17:46:41 +0200

    feat frontPage: add html and styles and images. [WIP] adjusting desktop styles

--- front-page.php
+++ front-page.php
@@ -1,14 +1,79 @@
 <?php get_header();?>
 
 <main>
-    <section>
-        <?php echo do_shortcode('[smartslider3 slider="4"]'); ?> 
-    </section>
-    <section class="content container">
-        <?php if(have_posts()) : while(have_posts()) : the_post();?>
-        <?php the_content();?>
-        <?php endwhile; else: endif;?>
-    </section>
+  <section>
+    <?php echo do_shortcode('[smartslider3 slider="4"]'); ?> 
+  </section> <!--Slider-->
+  <section class="content container">
+    <h1 class="title__frontpage">Venta de tejidos y telas online</h1>
+    <div class="categories-list">
+      <div class="category__div"> 
+        <div class="category__div__img">
+          <img src="<?php bloginfo('template_directory');?>/images/upholstery.jpg">
+          <div class="category__div__img--link">
+            <a href="#">Ver productos</a>
+          </div>
+        </div>
+        <div class="category__div__text">
+          <h2>Telas para tapizar</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+        </div>
+      </div> <!--Telas para tapizar-->
+      <div class="category__div">
+        <div class="category__div__img">
+          <img src="<?php bloginfo('template_directory');?>/images/outdoor.jpg">
+          <div class="category__div__img--link">
+            <a href="#">Ver productos</a>
+          </div>
+        </div>
+        <div class="category__div__text">
+          <h2>Telas para exterior</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+        </div>
+      </div> <!--Telas para exterior-->
+      <div class="category__div">
+        <div class="category__div__img">
+          <img src="<?php bloginfo('template_directory');?>/images/contract.jpg">
+          <div class="category__div__img--link">
+            <a href="#">Ver productos</a>
+          </div>
+        </div>
+        <div class="category__div__text">
+          <h2>Telas para contract</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+        </div>
+      </div> <!--Telas contract-->
+      <div class="category__div">
+        <div class="category__div__img">
+          <img src="<?php bloginfo('template_directory');?>/images/curtains.jpg">
+          <div class="category__div__img--link">
+            <a href="#">Ver productos</a>
+          </div>
+        </div>
+        <div class="category__div__text">
+          <h2>Telas para cortinas y visillos</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+        </div>
+      </div> <!--Telas Cortinas y visillos-->
+      <div class="category__div">
+        <div class="category__div__img">
+          <img src="<?php bloginfo('template_directory');?>/images/vynil-rug.jpg">
+          <div class="category__div__img--link">
+            <a href="#">Ver productos</a>
+          </div>
+        </div>
+        <div class="category__div__text">
+          <h2>Telas para alfombras de vinilo</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+        </div>
+      </div> <!--Telas Alfombras de vinilo-->
+    </div>
+  </section>
 </main>
 
 <?php get_footer();?>
